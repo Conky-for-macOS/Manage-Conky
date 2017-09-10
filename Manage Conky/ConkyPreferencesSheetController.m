19e0b154b5d1c7724cc126d8f50ec064eef5a3a4
npyl <n.pylarinos@hotmail.com>
2017-09-11 01:02:33 +0300

    add launchAgent support --- Missing from launchAgent creation is theme support... Coming soon
    I removed the library called AHLaunchCtl because I didn't know how to use it and decided to do it with the "hard" way

--- Manage Conky/ConkyPreferencesSheetController.m
+++ Manage Conky/ConkyPreferencesSheetController.m
@@ -8,7 +8,7 @@
 
 #import "ConkyPreferencesSheetController.h"
 
-#include "AHLaunchCtl/AHLaunchCtl/AHLaunchJob.h"
+#include <ServiceManagement/ServiceManagement.h>
 #include <unistd.h>
 
 
@@ -17,8 +17,6 @@
 @synthesize runConkyAtStartupCheckbox = _runConkyAtStartupCheckbox;
 @synthesize conkyConfigLocationTextfield = _conkyConfigLocationTextfield;
 
-
-NSString * kConkyAgentPlistLocation = @"/Library/LaunchAgents/org.npyl.conky.plist";
 
 NSString * kConkyAgentPlistName = @"org.npyl.conky.plist";
 NSString * kConkyConfigLocation = @"/Users/develnpyl/.conky";
@@ -89,24 +87,33 @@ NSString * kManageConkyPrefsPath = @"/Users/develnpyl/prefs.plist";
 {
     NSString * conkyAgentPlistPath = [[NSString alloc] initWithFormat:@"%@%@%@%@", @"/Users/", NSUserName(), @"/Library/LaunchAgents/", kConkyAgentPlistName ];
     
+    // TODO: add support for theme
+    
+    id objects[] = { @"org.npyl.conky", @[ @"/usr/local/bin/conky" ], [NSNumber numberWithBool:YES], [NSNumber numberWithBool:YES] };
+    id keys[] = { @"Label", @"ProgramArguments", @"RunAtLoad", @"KeepAlive"  };
+    NSUInteger count = sizeof(objects) / sizeof(id);
+    
+    NSDictionary * conkyAgentPlist = [[NSDictionary alloc] initWithObjects:objects forKeys:keys count:count];
+    
+    
     switch ([sender state]) {
         case 0:
             NSLog( @"Request to remove the Agent!" );
+            
+            // SMJobRemove() deprecated but suggested by Apple, see https://lists.macosforge.org/pipermail/launchd-dev/2016-October/001229.html
+            SMJobRemove( kSMDomainUserLaunchd, CFSTR("org.npyl.conky"), nil, YES, nil);
             
             unlink( [conkyAgentPlistPath UTF8String] );
             
             break;
         case 1:
             NSLog( @"Request to add the Agent!" );
-            
-            AHLaunchJob* job = [AHLaunchJob new];
-            job.Program = @"/usr/bin/conky";
-            job.Label = @"org.npyl.conky";
-            job.ProgramArguments = @[ @"-c", @"" ];
-            job.RunAtLoad = YES;
+ 
+            [conkyAgentPlist writeToFile:conkyAgentPlistPath atomically:YES];
             
             break;
         default:
+            ;
     }
 }
 
