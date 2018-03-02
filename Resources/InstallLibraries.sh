d06e5bf8e7b229675dcc7e2f8d204af9b920609c
npyl <n.pylarinos@hotmail.com>
2018-03-02 17:28:06 +0200

    Add supporting file from ConkyX project

---
+++ Resources/InstallLibraries.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  InstallLibraries.sh
+#  ConkyX
+#
+#  Created by Nikolas Pylarinos on 27/01/2018.
+#  Copyright © 2018 Nikolas Pylarinos. All rights reserved.
+
+/usr/local/bin/brew install cmake freetype gettext lua imlib2
