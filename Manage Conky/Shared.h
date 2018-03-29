38c8e60e614631e4af546c964f33582f9e4150ba
npyl <n.pylarinos@hotmail.com>
2018-03-30 00:59:35 +0300

    Add Shared.h

---
+++ Manage Conky/Shared.h
@@ -0,0 +1,21 @@
+//
+//  Shared.h
+//  Manage Conky
+//
+//  Created by npyl on 27/03/2018.
+//  Copyright © 2018 Nickolas Pylarinos. All rights reserved.
+//
+
+#ifndef Shared_h
+#define Shared_h
+
+//
+// Contains stuff used by more than one subprojects or files
+//
+
+#define HELPER_FINISHED_MESSAGE "I am done here..."     /* msg sent when Helper is quitting */
+
+#define kSMJOBBLESSHELPER_IDENTIFIER @"org.npyl.ManageConkySMJobBlessHelper"
+#define SMJOBBLESSHELPER_IDENTIFIER "org.npyl.ManageConkySMJobBlessHelper"
+
+#endif /* Shared_h */
