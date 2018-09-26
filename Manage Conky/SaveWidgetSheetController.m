eb2a793061d85974b625406ca4b99de8c769ad98
npyl <n.pylarinos@hotmail.com>
2018-09-26 21:29:57 +0300

    Improve some stuff

--- Manage Conky/SaveWidgetSheetController.m
+++ Manage Conky/SaveWidgetSheetController.m
@@ -15,9 +15,19 @@
     self = [super initWithWindowNibName:nibName];
     if (self)
     {
+        resourcesLocations = [NSMutableArray array];
         [_scriptView setSyntaxDefinitionName:@"lua"];
     }
     return self;
+}
+
+- (IBAction)clearButton:(id)sender
+{
+    _scriptView.string = @"";
+    _widgetCreatorField.stringValue = @"";
+    _widgetSourceField.stringValue = @"";
+    [resourcesLocations removeAllObjects];
+    previewLocation = [NSURL URLWithString:@""];
 }
 
 - (IBAction)cancelButton:(id)sender
@@ -29,27 +39,29 @@
 {
     NSOpenPanel *op = [NSOpenPanel openPanel];
     [op setAllowsMultipleSelection:NO];
-    [self.window beginSheet:op completionHandler:^(NSModalResponse returnCode) {
-        if (returnCode == NSModalResponseOK)
-        {
-            self->previewLocation = op.URL.filePathURL;
-        }
-    }];
+    [op setPrompt:@"Add"];
+    NSModalResponse res = [op runModal];
+
+    if (res == NSModalResponseOK)
+    {
+        self->previewLocation = op.URL;
+    }
 }
 
 - (IBAction)addResources:(id)sender
 {
     NSOpenPanel *op = [NSOpenPanel openPanel];
     [op setAllowsMultipleSelection:YES];
-    [self.window beginSheet:op completionHandler:^(NSModalResponse returnCode) {
-        if (returnCode == NSModalResponseOK)
+    [op setPrompt:@"Add"];
+    NSModalResponse res = [op runModal];
+    
+    if (res == NSModalResponseOK)
+    {
+        for (NSURL *url in [op URLs])
         {
-            for (NSURL *url in [op URLs])
-            {
-                [self->resourcesLocations addObject:url.filePathURL];
-            }
+            [self->resourcesLocations addObject:url];
         }
-    }];
+    }
 }
 
 - (IBAction)saveButton:(id)sender
@@ -57,24 +69,27 @@
     NSSavePanel *sp = [NSSavePanel savePanel];
     [sp setMessage:@"Choose Widget's Location"];
     [sp setNameFieldLabel:@"Widget's Name"];
+    [sp setPrompt:@"Create"];
     NSModalResponse res = [sp runModal];
     
     if (res == NSModalResponseOK)
     {
         NSString *widgetDirectory = sp.URL.path;
-        NSString *widgetConfig = [widgetDirectory stringByAppendingPathComponent:sp.nameFieldStringValue];
+        NSString *widgetConfig = [widgetDirectory stringByAppendingPathComponent:widgetDirectory.lastPathComponent];
         NSString *widgetSource = [widgetDirectory stringByAppendingPathComponent:@"source.txt"];
         NSString *widgetCreator = [widgetDirectory stringByAppendingPathComponent:@"creator.txt"];
         NSFileManager *fm = [NSFileManager defaultManager];
         NSError *error = nil;
         
-//        NSLog(@"%@", widgetDirectory);
-//        NSLog(@"%@", widgetConfig);
-        
-        [fm createDirectoryAtPath:widgetDirectory withIntermediateDirectories:YES attributes:nil error:&error];
+        /* delete any previous versions */
+        [fm removeItemAtPath:widgetDirectory error:nil];
+
+        /* create widget directory */
+        [fm createDirectoryAtPath:widgetDirectory withIntermediateDirectories:NO attributes:nil error:&error];
         if (error)
         {
             NSLog(@"%@", error);
+            return;
         }
         
         /* save conf */
@@ -89,11 +104,26 @@
         [fm createFileAtPath:widgetCreator
                     contents:[_widgetCreatorField.stringValue dataUsingEncoding:NSUTF8StringEncoding]
                   attributes:nil];
+
+        /* copy preview */
+        [fm copyItemAtPath:previewLocation.path toPath:widgetDirectory error:&error];
+        if (error)
+        {
+            NSLog(@"%@", error);
+        }
         
-        NSLog(@"%@", previewLocation);
-        NSLog(@"%@", resourcesLocations);
+        /* copy resources */
+        for (NSURL *resource in resourcesLocations)
+        {
+            [fm copyItemAtPath:resource.path toPath:widgetDirectory error:&error];
+            if (error)
+            {
+                NSLog(@"%@", error);
+            }
+        }
         
-        [self.window close];
+        /* open widget's directory */
+        [[NSWorkspace sharedWorkspace] openFile:widgetDirectory];
     }
 }
 
